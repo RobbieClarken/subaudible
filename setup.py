dc89daf8aa999a1e47f13e171695d8201a9c8e75
RobbieClarken <robbie.clarken@gmail.com>
2016-04-30 17:22:01 +1000

    Add license and fix travis testing

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup
 import re
 
+
 with open('subaudible/__init__.py') as file:
     version = re.search(r"__version__ = '(.*)'", file.read()).group(1)
+
+with open('README.rst') as file:
+    readme = file.read()
+
 
 setup(
     name='subaudible',
     version=version,
+    description='Display subtitles for audio picked up by the system microphone',
+    long_description=readme,
+    author='Robbie Clarken',
+    author_email='robbie.clarken@gmail.com',
+    url='https://github.com/RobbieClarken/subaudible',
+    license='MIT',
     packages=['subaudible'],
     install_requires=[
         'sounddevice',
